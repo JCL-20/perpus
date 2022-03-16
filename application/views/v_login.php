b96975935fa97edfadb6fe5845a23d158f752e0b
JCL-20 <12200528.bsi.ac.id>
2022-03-17 02:13:11 +0700

    perpus2

--- application/views/v_login.php
+++ application/views/v_login.php
@@ -31,14 +31,16 @@
   <div class="login-box-body">
     <p class="login-box-msg login">Login Sistem</p>
 
-    <form action="../../index2.html" method="post">
+    <?= $this->session->flashdata('info');?>
+
+    <form action="<?= base_url()?>login/proses_login" method="post">
       <div class="form-group has-feedback">
-        <input type="text" name="username" class="form-control" placeholder="Username">
+        <input type="text" name="username" class="form-control" placeholder="Username" required>
         <span class="glyphicon glyphicon-user form-control-feedback"></span>
       </div>
 
       <div class="form-group has-feedback">
-        <input type="password" name="passowrd" class="form-control" placeholder="Password">
+        <input type="password" name="password" class="form-control" placeholder="Password" required>
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
 
